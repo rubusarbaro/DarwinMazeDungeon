b633b7b7a0d733148c5945b5fa43fbaf2939b613
Saŭlo Rubusarbaro <61021296+rubusarbaro@users.noreply.github.com>
2023-10-05 18:42:07 -0600

    CHANGE #9
    1. a2/functional_objects.py
    "regular" and "title" methods created in "label" class.
    
    2. a2/main.py
    > "game_title" object created.
    > Comments added.
    
    3. a2/styles.py
    Module from "a1" added, but not used yet.

--- a2/functional_objects.py
+++ a2/functional_objects.py
@@ -62,28 +62,26 @@ class label :
 
     """
     Initialize an object of "label" class. The text containing the label has to be provided as string.
+    The available styles are "regular" and "title".
     """
 
-    def __init__(self, name: str) :
+    def __init__(self, text: str, style) :
 
         # Appends the created object to the list.
         list_labels.append(self)
 
-        self.name = name
+        self.text = text
         self.related_screen = object
-    
-    def set_as_text(self, screen: object, position_x: int, position_y: int) :
+        self.style = style
 
+    def regular(self) :
         """
-        It adds the text (label) to the assigned screen.
-        The target screen, and the position (x and y) of the label has to be provided.
-        "Screen" is object type. "position_x" and "position_y" are integer type.
+        Text without format, as it was initialized.
+        It returns the text in a list of two letters per item.
         """
-
-        self.related_screen = screen
 
         # Gets the lenght of the text and creates a list where every pair of letter are stored.
-        text_len = len(self.name)
+        text_len = len(self.text)
         text_as_list = []
 
         # It iterates a range from 0 to the half of the text length.
@@ -92,14 +90,47 @@ class label :
             a = i * 2
             b = a + 1
 
-            string = self.name[a] + self.name[b]
+            string = self.text[a] + self.text[b]
             text_as_list.append(string)
 
         # It determines if a text lenght is pair or not. If not, it concatenates the last letter to a blank space
         # and append them to the list.
         if text_len//2 != text_len/2 :
-            string = self.name[text_len-1] + " "
+            string = self.text[text_len-1] + " "
             text_as_list.append(string)
+
+        return text_as_list
+    
+    def title(self) :
+        """
+        Text in title style (uppercase and double space).
+        It returns the text in a list of one letter and one space per item.
+        """
+
+        # Gets the lenght of the text and creates a list where every pair of letter are stored.
+        text_len = len(self.text)
+        text_as_list = []
+
+        # It iterates a range from 0 to the half of the text length.
+        # It uses the half, because it's storing all the letter by pair, not single.
+        for i in range(0, text_len) :
+           string = self.text[i] + " "
+           text_as_list.append(string.upper())
+
+        return text_as_list
+
+    def set_as_text(self, screen: object, position_x: int, position_y: int) :
+
+        """
+        It adds the text (label) to the assigned screen.
+        The target screen, and the position (x and y) of the label has to be provided.
+        "Screen" is object type. "position_x" and "position_y" are integer type.
+        """
+
+        # Assigns the declared screen as related_screen to the object
+        self.related_screen = screen
+        # Defines the "text_as_list" values according to "label.regular" or "label.title"
+        text_as_list = self.style(self)
 
         # Substitutes every item in the list in the target position.
         for i in range(0, len(text_as_list)) :
--- a2/main.py
+++ a2/main.py
@@ -1,7 +1,15 @@
+# Import local modules
 from screen import blank
 from functional_objects import label, screen
 
+# Creates objects used in menu.
 home = screen(blank)
-author = label("Saŭlo Rubusarbaro © 2023")
+game_title = label("Darwin Maze Dungeon", label.title)
+author = label("Saŭlo Rubusarbaro © 2023", label.regular)
+
+# Set the objects to corresponding screen.
+game_title.set_as_text(home, 6, 5)
 author.set_as_text(home, 2, 27)
+
+#Print screens.
 home.print_screen()
